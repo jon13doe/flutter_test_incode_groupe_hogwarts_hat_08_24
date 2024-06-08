65a9af647b6329113c03d6c13ae160b4f0555c33
mike <jon13doe@gmail.com>
2024-06-08 14:30:07 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_test_incode_groupe_hogwarts_hat_08_24
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_test_incode_groupe_hogwarts_hat_08_24/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_test_incode_groupe_hogwarts_hat_08_24
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
